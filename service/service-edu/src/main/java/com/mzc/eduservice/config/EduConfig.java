7b6c85d78dbe0a0a9ea97c010ad2f617bb502435
jikeMisma <2889479657@qq.com>
2020-11-14 13:24:22 +0800

    Swagger实现条件查询

--- service/service-edu/src/main/java/com/mzc/eduservice/config/EduConfig.java
+++ service/service-edu/src/main/java/com/mzc/eduservice/config/EduConfig.java
@@ -2,6 +2,7 @@ package com.mzc.eduservice.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import io.swagger.annotations.Api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -27,5 +28,14 @@ public class EduConfig {
     @Bean
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
+    }
+
+    /**
+     * 分页插件
+     */
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
     }
 }
--- service/service-edu/src/main/java/com/mzc/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/mzc/eduservice/controller/EduTeacherController.java
@@ -1,16 +1,22 @@
 package com.mzc.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mzc.commonutils.R;
 import com.mzc.eduservice.entity.EduTeacher;
+import com.mzc.eduservice.entity.vo.TeacherQuery;
 import com.mzc.eduservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -53,6 +59,64 @@ public class EduTeacherController {
             return R.error();
         }
     }
+
+    //分页查询讲师方法
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R  pageListTeacher(
+            @ApiParam(name = "current",value = "当前页",required = true)
+            @PathVariable long current,@PathVariable long limit){
+        //创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        //调用方法实现分页
+        eduTeacherService.page(pageTeacher,null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+
+        Map map = new HashMap<>();
+        map.put("total",total);
+        map.put("records",records);
+        return  R.ok().data(map);
+
+    }
+
+    //4 条件查询带分页的方法
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
+                                  @RequestBody(required = false)  TeacherQuery teacherQuery) {
+        //创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+
+        //构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        // mybatis学过 动态sql
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空拼接条件
+        if(!StringUtils.isEmpty(name)) {
+            //构建条件
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create",end);
+        }
+
+        //调用方法实现条件查询分页
+        eduTeacherService.page(pageTeacher,wrapper);
+
+        long total = pageTeacher.getTotal();//总记录数
+        List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
+        return R.ok().data("total",total).data("rows",records);
+    }
+
 
 
 }
